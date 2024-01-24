2f5ff7f9a15fd3be928b437b0a772e3746f56a59
SaraAgnestrand <114142153+SaraAgnestrand@users.noreply.github.com>
2024-01-24 15:34:23 +0100

    comments

--- server/src/resources/user/user.model.js
+++ server/src/resources/user/user.model.js
@@ -1,6 +1,7 @@
 const { Schema, model, models } = require("mongoose");
 const Joi = require("joi");
 
+//Definierar Schema för User
 const UserSchema = new Schema({
     firstName: { type: String, required: true },
     lastName: { type: String, required: true },
@@ -9,8 +10,10 @@ const UserSchema = new Schema({
     isAdmin: { type: Boolean, required: true, default: false },
 });
 
+//Skaper eller hämtar modellen
 const UserModel = models.user || model("user", UserSchema);
 
+//Definierar Joi Schemat för validering
 const UserCreateValidationSchema = Joi.object({
     firstName: Joi.string().strict().required(),
     lastName: Joi.string().strict().required(),
@@ -19,5 +22,6 @@ const UserCreateValidationSchema = Joi.object({
     isAdmin: Joi.boolean().strict(),
 });
 
+//Exporterar Modellen, Joi Schemat och Schemat
 module.exports = { UserModel, UserCreateValidationSchema, UserSchema };
 
--- server/src/resources/user/user.router.js
+++ server/src/resources/user/user.router.js
@@ -3,11 +3,13 @@ const { registerUser, loginUser, logoutUser } = require("./user.controller");
 const { UserCreateValidationSchema } = require("./user.model");
 const { validate } = require("../../middlewares");
 
+//Skapar Express Router
 const userRouter = express.Router();
 
+//Definierar API-Rutter:
 userRouter.post("/register", validate(UserCreateValidationSchema), registerUser);
 userRouter.post("/loginUser", loginUser);
 userRouter.post("/logoutUser", logoutUser);
 
-
+//Exporterar Routern
 module.exports = userRouter;
